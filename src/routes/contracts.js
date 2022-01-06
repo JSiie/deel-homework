e8796bea06267c8bc4c85f138ce9134d2824d40d
Jonathan Sanchez <me-gitlab@jonathan-sanchez.com>
2022-01-06 18:58:20 +0100

    ending contracts api

--- src/routes/contracts.js
+++ src/routes/contracts.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {sequelize} = require('../model')
-const {getProfile} = require('../middleware/getProfile')
+const {sequelize} = require('../model');
+const { Op } = require("sequelize");
+const {getProfile} = require('../middleware/getProfile');
 var router = express.Router();
 router.use(bodyParser.json());
 
@@ -11,11 +12,30 @@ router.use(bodyParser.json());
  router.get('/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
+    const {profile} = req
     const contract = await Contract.findOne({where: {id}})
     if(!contract) return res.status(404).end()
     // We now test if profile is either the client or contractor one. Else we return 401 error (not authorized)
     // NOTA: we may think about returning 404 for security purpose if we want to secure it at max: giving a 401 error is already giving an information to hacker: the contract exists.
-    if(contract.ClientId != req.profile.id && contract.ContractorId != req.profile.id) return res.status(401).end()
+    if(contract.ClientId != profile.id && contract.ContractorId != profile.id) return res.status(401).end()
     res.json(contract)
 })
+
+/**
+ * @returns contracts belonging to the caller (through his profile id)
+ * In case of empty list, should still return a 200 http answer
+ */
+ router.get('/',getProfile ,async (req, res) =>{
+    const {Contract} = req.app.get('models')
+    const {profile} = req
+    const contract = await Contract.findAll({where: {
+                                                        [Op.or]: [
+                                                            { ClientId: profile.id },
+                                                            { ContractorId: profile.id }
+                                                        ]
+                                                    }
+    })
+    res.json(contract)
+})
+
 module.exports = router;
